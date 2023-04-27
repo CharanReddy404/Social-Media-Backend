3d32890cc74aea2d53635159b283ac57c3f8a10d
CharanReddy404 <charanlucky6143@gmail.com>
2023-04-27 13:10:08 +0530

    options removed

--- index.js
+++ index.js
@@ -30,14 +30,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-
-const options = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
-app.use(cors(options));
+app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 // FILE STORAGE
